7b88ab4aac6bc3bae44b5d8cf5dda5ea28ff6344
Jetlink <kaan@jetlink.io>
2017-08-25 16:31:00 +0300

    Update jetlink.sample.config.js

--- jetlink.sample.config.js
+++ jetlink.sample.config.js
@@ -1,19 +1,17 @@
-  
-        Jetlink.Settings.Widget.OnlineWidgetUrl = "http://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/chat-2-icon.png";
-        Jetlink.Settings.Widget.SendButtonText = "Send the message";
-        Jetlink.Settings.Widget.MessageTextBoxPlaceholder = "Type Here";
+Jetlink.Options.LauncherType = "cornered";
+Jetlink.Options.ShowEmailAndPhoneRequest = true;
+Jetlink.Options.ThemeColor = "#900";
+Jetlink.Options.HideLauncherWhenChatWindowOpen = false; 
+Jetlink.Options.IsVisitorEmailOrPhoneRequired = false;
+Jetlink.Options.Language = "en";
 
-        Jetlink.Rules.IsVisitorEmailRequired = true;
-        Jetlink.Settings.Widget.WelcomeText = "Welcome to our live support system."
-        Jetlink.Settings.Widget.ChatWindowBackgroundColor = "#cccccc";
-        Jetlink.Init("jetlink-web-chat-11111111111", "1111111111333333333332222222");
-        var user = {
-            SourceUserId: "3234234324324",
-            Email: "testuser@jetlink.io",
-            Phone: "+905321231212",
-            Name: "Name here",
-            Surname: "Surname here"
-        };
-        Jetlink.SetUser(user);
+var user = {
+    SourceUserId: "3234234324324",
+    Email: "testuser@jetlink.io",
+    Phone: "+905321231212",
+    Name: "Name here",
+    Surname: "Surname here"
+};
+Jetlink.SetUser(user);
 
-        Jetlink.Localize("tr");
+Jetlink.Init("YOUR-APP-ID", "YOUR-APP-TOKEN"); 
