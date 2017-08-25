1793a55f939c51fb505135c207fb6215aa68fd33
Jetlink <kaan@jetlink.io>
2017-08-25 16:38:05 +0300

    Update jetlink.sample.config.js

--- jetlink.sample.config.js
+++ jetlink.sample.config.js
@@ -1,7 +1,7 @@
 Jetlink.Options.LauncherType = "cornered";
 Jetlink.Options.ShowEmailAndPhoneRequest = true;
 Jetlink.Options.ThemeColor = "#900";
-Jetlink.Options.HideLauncherWhenChatWindowOpen = false; 
+Jetlink.Options.HideLauncherWhenChatWindowIsOpen = false; 
 Jetlink.Options.IsVisitorEmailOrPhoneRequired = false;
 Jetlink.Options.Language = "en";
 
